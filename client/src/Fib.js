ea60018ffd653ff469877e144bf1a868a51d675b
usnaik <upendra_naik@yahoo.com>
2019-05-01 13:38:58 +0530

    Initial commit.

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -34,18 +34,18 @@ class Fib extends Component {
     this.setState({ index: '' });
   };
 
-  // renderSeenIndexes() {
-  //   return this.state.seenIndexes.map(({ number }) => number).join(', ');
-  // }
-
   renderSeenIndexes() {
-    if (this.state && this.state.seenIndexes && Array.isArray(this.state.seenIndexes)) {
-      return this.state.seenIndexes.map(({ number }) => number).join(', ');
-    }
-    else {
-      return null;
-    }
+    return this.state.seenIndexes.map(({ number }) => number).join(', ');
   }
+
+  // renderSeenIndexes() {
+  //   if (this.state && this.state.seenIndexes && Array.isArray(this.state.seenIndexes)) {
+  //     return this.state.seenIndexes.map(({ number }) => number).join(', ');
+  //   }
+  //   else {
+  //     return null;
+  //   }
+  // }
 
   renderValues() {
     const entries = [];
